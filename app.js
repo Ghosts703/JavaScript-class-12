1e8268897aa58386e21274a838ffeec8ce8fbdcd
Ghosts703 <fk254945+GitHub@gmail.com>
2024-01-14 17:12:54 +0500

    14 jan 2024

--- app.js
+++
@@ -1,4 +0,0 @@
-function para() {
-  var text1 = "Text after the read more anchor.";
-  document.getElementById("anc1").innerHTML = text1;
-}
---
+++ practice code 1 - Read More functionality code/app.js
@@ -0,0 +1,5 @@
+function para() {
+  var text1 = "Text after the read more anchor.";
+  document.getElementById("para1").innerHTML += text1;
+  document.getElementById("anc1").innerHTML = '';
+}
