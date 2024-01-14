be97f1af8d4bdfc386c4fa0476df705c442b722c
Ghosts703 <fk254945+GitHub@gmail.com>
2024-01-14 18:00:49 +0500

    14 jan 2024

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+function addTodo() {
+  var getInp = document.getElementById("inp1");
+  // console.log(getInp.value);
+  var createLi = document.createElement("li");
+  var liText = document.createTextNode("getInp.value");
+}
